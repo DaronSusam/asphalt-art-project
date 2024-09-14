8df8de00c0dd30783dd0f40a8f3e9ada68d045fc
DaronSusam <dsusam0001@mymail.lausd.net>
2024-09-13 22:21:12 -0700

    All java code for Golden Painter

---
+++ NeighboorhoodRunner.java
@@ -0,0 +1,17 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+  Muralpainter adam = new Muralpainter();
+    ApplePainter daron = new ApplePainter();
+
+    //Paint Background
+    adam.paintBackground("Gray",16);
+
+      daron.drawOutline();
+    
+
+    
+  }
+}
